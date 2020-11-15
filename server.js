8058c528bfbcc68ccae33c910afdbc0e9249c055
Fernando Maia <fermaiasoares@aol.com>
2020-11-15 19:46:38 -0300

    .

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + 'dist/navigatron'));
+
+app.get('/*', function(request, response) {
+  response.sendFile(path.join(__dirname + '/dist/navigatron/index.html'));
+})
+
+app.listen(process.env.PORT || 8080);
